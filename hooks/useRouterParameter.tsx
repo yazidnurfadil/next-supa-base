60ffc2f256c86dcdf28f8bc35b89bf09b8a39182
Yazid Nurfadil <yazidnurfadil@gmail.com>
2024-11-15 11:15:31 +0700

    #0 | Yazid Nurfadil | chore: update router parameter custom hook

--- hooks/useRouterParameter.tsx
+++ hooks/useRouterParameter.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 import { createQueryString } from "@/utils";
@@ -7,13 +8,35 @@ const useRouterParameter = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  const updateQueryString = (name: string, value: string) => {
+  const updateQueryString = useCallback((name: string, value: string) => {
     router.replace(
       pathname + "?" + createQueryString(name, value, searchParams.toString())
     );
-  };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  return { updateQueryString, router, pathname, searchParams };
+  const deleteQueryParameter = useCallback(
+    (query: string | string[]) => {
+      const params = new URLSearchParams(searchParams.toString());
+      if (Array.isArray(query)) {
+        query.forEach((q) => params.delete(q));
+      } else {
+        params.delete(query);
+      }
+      const newParams = params.toString();
+      router.replace(pathname + "?" + newParams);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  return {
+    updateQueryString,
+    deleteQueryParameter,
+    router,
+    pathname,
+    searchParams,
+  };
 };
 
 export default useRouterParameter;
