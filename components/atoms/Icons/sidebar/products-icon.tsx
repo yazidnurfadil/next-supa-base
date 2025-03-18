f67244f7cdfdb92e2aad912a1994982819712389
Yazid Nurfadil <yazidnurfadil@gmail.com>
2025-03-19 01:35:10 +0700

    #0 | Yazid Nurfadil | chore: fix dependencies

--- components/atoms/Icons/sidebar/products-icon.tsx
+++ components/atoms/Icons/sidebar/products-icon.tsx
@@ -1,4 +1,4 @@
-export const ProductsIcon = () => {
+export const ProductsIcon = (props: React.SVGAttributes<SVGElement>) => {
   return (
     <svg
       width="24"
@@ -6,11 +6,12 @@ export const ProductsIcon = () => {
       fill="none"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <path
-        fill="#969696"
         fillRule="evenodd"
         clipRule="evenodd"
+        fill="currentColor"
         className="fill-default-400"
         d="M20 6H16V4C16 2.89 15.11 2 14 2H10C8.89 2 8 2.89 8 4V6H4C2.89 6 2.01 6.89 2.01 8L2 19C2 20.11 2.89 21 4 21H20C21.11 21 22 20.11 22 19V8C22 6.89 21.11 6 20 6ZM14 6H10V4H14V6Z"
       />
