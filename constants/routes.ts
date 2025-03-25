bc9454f7ec05e27070bf4abd2d7bb80e88af266f
Yazid Nurfadil <yazidnurfadil@gmail.com>
2025-03-26 01:33:15 +0700

    #0 | Yazid Nurfadil | chore: add routes constant

---
+++ constants/routes.ts
@@ -0,0 +1,5 @@
+export const ROUTE_HOME = "/";
+export const ROUTE_DASHBOARD = "/dashboard";
+export const ROUTE_DASHBOARD_ERROR = "/dashboard-error";
+export const ROUTE_ACCOUNTS = "/accounts";
+export const ROUTE_CUSTOMERS = "/costomers";
